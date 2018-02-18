19deae80b2891294b20264fcecfc5bd878d0cd18
Michal Kapiczynski <michal.kapiczynski@sirenia.eu>
2018-02-18 13:38:29 +0100

    Initial project setup

---
+++ Assets/Scripts/Script.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using OpenCVForUnity;
+
+public class Script : MonoBehaviour {
+
+	Mat cameraImageMat;
+	Mat cameraImageMatBlur = new Mat();
+
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		MatDisplay.SetCameraFoV (41.0f);
+		Image cameraImage = CameraDevice.Instance.GetCameraImage (Image.PIXEL_FORMAT.RGBA8888);
+		if (cameraImage != null) {
+			if (cameraImageMat == null) {
+				cameraImageMat = new Mat (cameraImage.Height, cameraImage.Width, CvType.CV_8UC4);
+			}
+
+			cameraImageMat.put (0, 0, cameraImage.Pixels);
+
+			MatDisplay.DisplayMat (cameraImageMat, MatDisplaySettings.BOTTOM_LEFT);
+			Imgproc.blur(cameraImageMat, cameraImageMatBlur, new Size (16,16));
+			MatDisplay.DisplayMat (cameraImageMatBlur, MatDisplaySettings.FULL_BACKGROUND);
+		}
+
+	}
+
+
+
+}
